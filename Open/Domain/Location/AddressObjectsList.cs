64b70e880b9955639d250b178a770f898be874b1
stella.leego <stella.leego@hotmail.com>
2018-04-30 13:46:02 +0300

    Siis kui need õpetaja testide pildid tulid, meil üks test ei tule välja

---
+++ Open/Domain/Location/AddressObjectsList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Open.Core;
+using Open.Data.Location;
+
+namespace Open.Domain.Location
+{
+    public class AddressObjectsList : PaginatedList<IAddressObject>
+    {
+        public AddressObjectsList(IEnumerable<AddressDbRecord> items, RepositoryPage page) : base(page) {
+            if (items is null) return;
+            foreach (var dbRecord in items) {
+                Add(AddressObjectFactory.Create(dbRecord));
+            }
+        }
+    }
+}
