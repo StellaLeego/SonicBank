13b3b49b984d51aae0651c3404b0790d24845333
stella.leego <stella.leego@hotmail.com>
2018-05-02 01:13:37 +0300

    See koht kus kõik testid üeaksid korras olema

---
+++ Open/Domain/Location/TelecomDeviceRegistationObjectFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Open.Data.Location;
+
+namespace Open.Domain.Location
+{
+    public static class TelecomDeviceRegistationObjectFactory
+    {
+        public static TelecomDeviceRegistrationObject Create(GeographicAddressObject address,
+            TelecomAddressObject device, DateTime? validFrom = null, DateTime? validTo = null) {
+            var o = new TelecomDeviceRegistrationDbRecord {
+                Address = address?.DbRecord,
+                Device = device?.DbRecord,
+                ValidFrom = validFrom ?? DateTime.MinValue,
+                ValidTo = validTo ?? DateTime.MaxValue
+            };
+            o.AddressID = o.Address.ID;
+            o.DeviceID = o.Device.ID;
+            return  new TelecomDeviceRegistrationObject(o);
+        }
+    }
+}
---
+++ Open/Domain/Location/TelecomDeviceRegistrationObject.cs
@@ -0,0 +1,15 @@
+﻿using Open.Data.Location;
+using Open.Domain.Common;
+
+namespace Open.Domain.Location
+{
+    public class TelecomDeviceRegistrationObject : TemporalObject<TelecomDeviceRegistrationDbRecord> {
+        public readonly TelecomAddressObject Device;
+        public readonly GeographicAddressObject Address;
+
+        public TelecomDeviceRegistrationObject(TelecomDeviceRegistrationDbRecord dbRecord) : base(dbRecord) {
+            Address = AddressObjectFactory.Create(DbRecord.Address) as GeographicAddressObject;
+            Device = AddressObjectFactory.Create(DbRecord.Device) as TelecomAddressObject;
+        }
+    }
+}
