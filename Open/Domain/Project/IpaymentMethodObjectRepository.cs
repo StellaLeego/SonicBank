53e059852526bf054fc57a1b2b785f10d0bbbea2
stella.leego <stella.leego@hotmail.com>
2018-05-03 12:22:49 +0300

    :)

---
+++ Open/Domain/Project/IpaymentMethodObjectRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Open.Core;
+using Open.Data.Project;
+
+namespace Open.Domain.Project
+{
+    public interface IPaymentMethodObjectRepository : IObjectsRepository<IPaymentMethodObject, PaymentMethodDbRecord> {
+        Task<IPaginatedList<IPaymentMethodObject>> GetDevicesList();
+    }
+}
