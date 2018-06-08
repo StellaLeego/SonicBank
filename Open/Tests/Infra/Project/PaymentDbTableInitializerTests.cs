f244aee001f30a944430fc237fd513947d411586
stella.leego <stella.leego@hotmail.com>
2018-06-08 18:08:01 +0300

    Testid siin, testid seal, testid iga nurga peal

---
+++ Open/Tests/Infra/Project/PaymentDbTableInitializerTests.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Open.Infra.Project;
+
+namespace Open.Tests.Infra.Project
+{
+    class PaymentDbTableInitializerTests : BaseTests
+    {
+        [TestInitialize]
+        public override void TestInitialize()
+        {
+            base.TestInitialize();
+            type = typeof(PaymentDbTableInitializer);
+        }
+
+        [TestMethod]
+        public void CantInitializeTest()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void InitializeTest()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
